f8d14f75930185f2e6824c1a749134cadf05175b
Naqiyah Lakdawala <naqiyah.lakdawala@focalworks.in>
2017-12-08 14:35:29 +0530

    itemsrepository and others

---
+++ packages/Checklist/src/Models/Item.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Naqiyah
+ * Date: 12/8/2017
+ * Time: 11:31 AM
+ */
+
+namespace Checklist\src\Models;
+
+use App\Item;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    //
+}
--- packages/Checklist/src/Repositories/ItemRepository.php
+++ packages/Checklist/src/Repositories/ItemRepository.php
@@ -4,5 +4,7 @@ namespace Checklist\src\Repositories;
 
 interface ItemRepository
 {
+public function createItem();
 
+public function getall();
 }
